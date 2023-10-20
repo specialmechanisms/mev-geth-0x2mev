717069a466d48cc91167831fd5b4c2b74d87513e
dergarcon <nm@rook.fi>
2023-10-19 18:38:44 -0600

    minor change

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -245,7 +245,7 @@ var allCurvePools []string
 var err error
 
 func init() {
-	fmt.Println("nickdebug NewHeads: init() called - aad012j")
+	fmt.Println("nickdebug NewHeads: init() called - bbbqadp")
 	numWorkers = runtime.NumCPU() - 1
 	if numWorkers < 1 {
 		numWorkers = 1 // Ensure at least one worker
