ce4aaa9ffb8d58884efb5a729e03e0ebc0dd0372
dergarcon <nm@mechanism.org>
2024-01-16 06:37:14 -0600

    created own namespace for smg_call and smg_estimateGas

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -245,7 +245,7 @@ var allCurvePools []string
 var err error
 
 func init() {
-	fmt.Println("nickdebug NewHeads: init() called - 111blue")
+	fmt.Println("nickdebug NewHeads: init() called - 222green")
 	numWorkers = runtime.NumCPU() - 1
 	if numWorkers < 1 {
 		numWorkers = 1 // Ensure at least one worker
